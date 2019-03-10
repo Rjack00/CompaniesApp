cfd56920995df630d9acd7de537ff78df482042a
Rjack00 <rjackson77@comcast.net>
2019-03-10 15:04:28 -0600

    ajax attempts

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,5 +1,8 @@
 import React from 'react';
+/////// Needed for DaveCeddia Tutorial //////////////
+//import ReactDOM from 'react-dom';
 import axios from 'axios';
+
 
 export default class Companies extends React.Component {
     state = {
@@ -9,25 +12,73 @@ export default class Companies extends React.Component {
     };
 
     componentDidMount() {
+
+///////// DaveCeddia AJAX/REACT tutorial couldn't make work ///////////////
+
+      // axios.get(`https://vast-shore-53950.herokuapp.com./companies.json`)
+      //   .then(res => {
+      //     const companies = res.data.data.children.map(obj => obj.data);
+      //     this.setState({companies});
+      //   });
+      // }
+
+//////////  KIM'S CODE works in console but not sure how to setState & render ////////////////
+
         axios.get(`https://vast-shore-53950.herokuapp.com`)
-        .then(res => res.json())
-        .then(
-            (result) => {
-            this.setState({ 
-                isLoaded: true,
-                companies: result.companies
-             });
-            },
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
-            }
-        )
+        // .then(function (response) {
+        //     this.setState({
+        //         isLoaded: true,
+        //         companies: response.companies
+        //     })
+        // })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     }
+////////////// REACT TUTORIAL CODE-1 couldn't make work //////////////
+     
+        // axios.get(`https://vast-shore-53950.herokuapp.com`)
+        // .then(
+        //   (result) => {
+        //     this.setState({
+        //       isLoaded: true,
+        //       companies: result.companies
+        //     });
+        //   },
+        //   // Note: it's important to handle errors here
+        //   // instead of a catch() block so that we don't swallow
+        //   // exceptions from actual bugs in components.
+        //   (error) => {
+        //     this.setState({
+        //       isLoaded: true,
+        //       error
+        //     });
+        //   }
+        // )
+    // }
     render() {
-        const { error, isLoaded, companies } = this.state;
+
+      /////// DaveCeddia AJAX/REACT tutorial /////////////////
+
+      // return (
+      //   <div>
+      //     <h1>{`/${this.props.companies}`}</h1>
+      //     <ul>
+      //       {this.state.companies.map(companies =>
+      //         <li Key={companies.id}>{companies.companyName}</li>
+      //         )}
+      //     </ul>
+      //  </div>
+      //       );
+      //     }
+      //   }
+
+      //////// REACT TUTORIAL CODE-1 only able to render Error: & Loading...////
+    
+          const { error, isLoaded, companies } = this.state;
         if (error) {
             return <div>Error: {error.messsage}</div>;
         } else if (!isLoaded) {
@@ -35,13 +86,36 @@ export default class Companies extends React.Component {
         }else {
             return(
                 <ul>
-                    {companies.map(companies => (
-                        <li key={companies.companyName}>
+                    {companies.map(item => (
+                        <li key={item.companyName}>
                         {companies.companyName}
                         </li>
                     ))}
                 </ul>
             );
         }
-        }
-        }
+      }
+    }
+
+////////// My attempt but only rendered <h1> /////////////
+       
+// const renderedCompanies = this.state.companies;
+//         return ( 
+//           <div>
+//             <h1>Rendered Companies:</h1>
+//             <ul>
+//               {renderedCompanies}
+//             </ul>
+//           </div>
+//         );
+//     }
+//   }
+
+  ////////// DaveCeddia AJAX/REACT tutorial couldn't make work ///////
+
+    // ReactDOM.render(
+    //   <Companies />,
+    //   document.getElementById('root')
+    // );
+
+
